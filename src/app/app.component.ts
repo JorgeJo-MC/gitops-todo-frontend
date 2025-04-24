07a4e2f17a9315f5aea48e7c6bc3dfa3a7d8b042
Jorge Jo <jjo@mangochango.com>
2025-04-24 17:27:15 -0600

    Development (#4)
    * ci: update image tag to 2f97f38
    
    * feat: todo (#3)
    
    * ci: update image tag to e1a62bd
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [CommonModule, FormsModule]
 })
 export class AppComponent {
-  title = 'todo-app-dev';
+  newTodo = '';
+  todos: string[] = [];
+
+  addTodo() {
+    if (!this.newTodo.trim()) return;
+    this.todos.push(this.newTodo.trim());
+    this.newTodo = '';
+  }
+
+  removeTodo(todo: string) {
+    this.todos = this.todos.filter(t => t !== todo);
+  }
 }
